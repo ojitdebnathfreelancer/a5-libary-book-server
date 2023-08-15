45a77d8a1077c600e8fc536150f24f02a3e7cfb5
unknown <ojitdebnathfreelancer.com>
2023-08-15 21:32:17 +0600

    full server done

---
+++ index.js
@@ -0,0 +1,108 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.r7d25w3.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const booksCollection = client.db("practiceDB").collection("books");
+    const userCollection = client.db("practiceDB").collection("usersCol");
+
+    //<<------------ books route start---------------->>//
+
+    app.get("/books", async (req, res) => {
+      const result = await booksCollection
+        .find({})
+        .sort({ $natural: -1 })
+        .toArray();
+      res.send(result);
+    });
+
+    app.get("/book/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await booksCollection.findOne(filter);
+      res.send(result);
+    });
+    app.post("/book", async (req, res) => {
+      const book = req.body;
+      const result = await booksCollection.insertOne(book);
+      res.send(result);
+    });
+    app.delete("/book/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await booksCollection.deleteOne(filter);
+      res.send(result);
+    });
+
+    app.put("/book/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const data = req.body;
+      const updateDoc = {
+        $set: data,
+      };
+      const result = await booksCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    // < < ..................Create Review ............ >>
+
+    app.put("/review/:id", async (req, res) => {
+      const review = req.body;
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const result = await booksCollection.updateOne(
+        filter,
+        {
+          $push: {
+            reviews: review,
+          },
+        },
+        options
+      );
+      res.send(result);
+    });
+
+    // <<------------ books route end---------------->>//
+    // <<------------ user route start---------------->>//
+
+    app.get("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const result = await userCollection.findOne({ email });
+      res.send(result);
+    });
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+
+run().catch((err) => console.error(err));
+
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running...on ${port}`);
+});
